78fc2e79d82521b4e3d1adefafcea99f9c0f8488
Ben Langfeld <ben@langfeld.me>
2014-10-12 18:36:14 -0300

    Creation of credentials & basic secret checking

--- index.js
+++ index.js
@@ -4,14 +4,28 @@
 
 var crypto = require('crypto');
 
+var required = function(name) {
+  throw(Error, 'requires ' + name);
+};
+
+var sign = function(message, secret) {
+  var hmac = crypto.createHmac('sha1', secret);
+  hmac.update(message);
+  return hmac.digest('base64');
+};
+
 exports.createSecret = function () {
   return crypto.randomBytes(64).toString('hex');
 };
 
-exports.createCredentials = function () {
+exports.createCredentials = function (args) {
+  var secret = args.secret || required('secret')
+    , username = 'foo'
+    ;
 
+  return {username: username, password: sign(username, secret)};
 };
 
-exports.testCredentials = function () {
-
+exports.testCredentials = function (credentials, secret) {
+  return credentials.password == sign(credentials.username, secret);
 };
--- tests/unit.js
+++ tests/unit.js
@@ -13,3 +13,43 @@ describe('creating a secret', function () {
     expect(mojoauth.createSecret()).to.have.length.above(20);
   });
 });
+
+describe('creating and testing credentials', function () {
+  describe('without an asserted ID', function () {
+    var credentials
+      , secret = mojoauth.createSecret()
+      ;
+
+    beforeEach(function() {
+      credentials = mojoauth.createCredentials({secret: secret});
+    });
+
+    it('raises if no secret is given', function () {
+      expect(function () { mojoauth.createCredentials({}); }).to.throw('requires secret');
+    });
+
+    describe('for the generated credentials', function () {
+      it('tests true', function () {
+        expect(mojoauth.testCredentials(credentials, secret)).to.be.true;
+      });
+    });
+
+    describe('with an incorrect password', function () {
+      it('tests false', function () {
+        expect(mojoauth.testCredentials({ username: credentials.username, password: 'foobar' }, secret)).to.be.false;
+      });
+    });
+
+    describe('with a different secret', function () {
+      it('tests false', function () {
+        expect(mojoauth.testCredentials(credentials, 'something_else')).to.be.false;
+      });
+    });
+
+    describe('when attempting to extend the expiration', function () {
+      it('tests false', function () {
+        expect(mojoauth.testCredentials({ username: "#{(Time.now + 100_000).to_i}", password: credentials.password }, secret)).to.be.false;
+      });
+    });
+  });
+});
